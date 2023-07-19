64c9a5ae293d89a7f7a536dcdf003d5be358f6db
GrozmanGit <101990647+GrozmanGit@users.noreply.github.com>
2023-07-18 22:17:07 -0500

    Create Texture.h

---
+++ Texture/Texture.h
@@ -0,0 +1,27 @@
+#ifndef TEXTURE_CLASS_H
+#define TEXTURE_CLASS_H
+
+#include<glad/glad.h>
+#include<stb/stb_image.h>
+
+#include"shaderClass.h"
+
+class Texture
+{
+private:
+
+public:
+	GLuint ID;
+	GLenum type;
+	Texture(const char* image, GLenum texType, GLenum slot, GLenum format, GLenum pixelType);
+
+	// assigns a texture unit to a texture
+	void texUnit(Shader& shader, const char* uniform, GLuint unit);
+	// binds texture
+	void Bind();
+	// unbinds texture
+	void Unbind();
+	// deletes texture
+	void Delete();
+};
+#endif
